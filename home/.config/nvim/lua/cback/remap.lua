732c5672deeee9b2b3a491db5cc99399f8f9641f
cbackas <zgibson@live.com>
2023-07-27 08:25:00 -0500

    shortcut to format code

--- home/.config/nvim/lua/cback/remap.lua
+++ home/.config/nvim/lua/cback/remap.lua
@@ -9,6 +9,9 @@ vim.keymap.set({ 'n', 'v', 'x' }, 'h', ';', { noremap = true })
 -- Keymaps for better default experience
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Map to format the current buffer
+vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
