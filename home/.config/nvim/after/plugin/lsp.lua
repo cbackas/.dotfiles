b5524ea9c67ea20fba1ea1302d68e7cbf0109881
cbackas <zgibson@live.com>
2023-07-27 23:09:03 -0500

    add yaml config

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -75,6 +75,36 @@ local servers = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
     },
+  },
+  yamlls = {
+    yaml = {
+      format = {
+        enable = true,
+      },
+      hover = true,
+      completion = true,
+
+      customTags = {
+        "!fn",
+        "!And",
+        "!If",
+        "!Not",
+        "!Equals",
+        "!Or",
+        "!FindInMap sequence",
+        "!Base64",
+        "!Cidr",
+        "!Ref",
+        "!Ref Scalar",
+        "!Sub",
+        "!GetAtt",
+        "!GetAZs",
+        "!ImportValue",
+        "!Select",
+        "!Split",
+        "!Join sequence"
+      },
+    },
   }
 }
 
