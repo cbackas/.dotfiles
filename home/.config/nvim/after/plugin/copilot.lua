2af8c1eaf9b798272eef97de31b79c948322ae3e
Zac Gibson <zgibson@securityesys.com>
2023-08-14 20:19:15 -0500

    move copilot settings out

---
+++ home/.config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,21 @@
+require("copilot").setup({
+  panel = {
+    enabled = false
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    keymap = {
+      accept = "<tab>",
+    }
+  },
+})
+
+local copilot_suggestion = require("copilot.suggestion")
+vim.keymap.set('i', '<Tab>', function()
+  if copilot_suggestion.is_visible() then
+    copilot_suggestion.accept_line()
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, true, true), 'n', false)
+  end
+end, { noremap = true })
--- home/.config/nvim/lua/cback/lazy.lua
+++ home/.config/nvim/lua/cback/lazy.lua
@@ -141,20 +141,6 @@ require('lazy').setup({
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        panel = {
-          enabled = false
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          keymap = {
-            accept = "<tab>",
-          }
-        }
-      })
-    end,
   },
 
   { "mbbill/undotree" },
