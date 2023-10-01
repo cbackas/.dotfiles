07701672f295bcc9c46c52924ae0102ddd0e675f
cbackas <zgibson@live.com>
2023-10-01 08:34:03 -0500

    just use the config_builder

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,13 +1,9 @@
 local wezterm = require 'wezterm'
 
--- In newer versions of wezterm, use the Wez_Conf_builder which will
--- help provide clearer error messages
-_G.Wez_Conf = {} -- global config variable
-if wezterm.Wez_Conf_builder then
-  Wez_Conf = wezterm.config_builder()
-end
+-- Wez_Conf is a global variable that holds all the configuration
+_G.Wez_Conf = wezterm.config_builder()
 
--- Wez_Conf is modified in the following files
+-- build up the Wez_Conf values from the other files
 require("set")
 require("appearance")
 require("remap")
