bdedd6fbad65065c38ade514431541f231a3c50c
cbackas <zgibson@live.com>
2023-10-02 21:21:31 -0500

    scrollback

--- home/.config/wezterm/set.lua
+++ home/.config/wezterm/set.lua
@@ -1,10 +1,13 @@
 -- general settings
+
 Wez_Conf.front_end = "WebGpu"
 Wez_Conf.freetype_load_target = "HorizontalLcd"
-
 Wez_Conf.animation_fps = 60
 Wez_Conf.max_fps = 120
 
+Wez_Conf.scrollback_lines = 100000
+
+-- use a unix domain so sessions outlast wezterm
 Wez_Conf.unix_domains = {
   {
     name = 'unix',
