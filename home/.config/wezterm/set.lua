dbdf2b657a7927888150dbf300a786f35463eefe
cbackas <zgibson@live.com>
2023-10-02 21:23:23 -0500

    ok lets b reasonable

--- home/.config/wezterm/set.lua
+++ home/.config/wezterm/set.lua
@@ -5,7 +5,7 @@ Wez_Conf.freetype_load_target = "HorizontalLcd"
 Wez_Conf.animation_fps = 60
 Wez_Conf.max_fps = 120
 
-Wez_Conf.scrollback_lines = 100000
+Wez_Conf.scrollback_lines = 10000
 
 -- use a unix domain so sessions outlast wezterm
 Wez_Conf.unix_domains = {
