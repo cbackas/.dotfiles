0cddb9993dc87dcab3245a9a471daba87f4e3f82
cbackas <zgibson@live.com>
2023-10-02 20:47:46 -0500

    enable unix domain

--- home/.config/wezterm/set.lua
+++ home/.config/wezterm/set.lua
@@ -1,4 +1,15 @@
 -- general settings
 Wez_Conf.front_end = "WebGpu"
+Wez_Conf.freetype_load_target = "HorizontalLcd"
+
 Wez_Conf.animation_fps = 60
 Wez_Conf.max_fps = 120
+
+Wez_Conf.unix_domains = {
+  {
+    name = 'unix',
+    local_echo_threshold_ms = 10,
+  },
+}
+-- enable unix domain socket by default
+Wez_Conf.default_gui_startup_args = { 'connect', 'unix' }
