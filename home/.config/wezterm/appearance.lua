1aad3f377734d9a9b3478501a1cd74321f45be1a
Zac Gibson <zgibson@securityesys.com>
2023-10-17 12:43:24 -0500

    change themes cuz i need to be able to see all the colors

--- home/.config/wezterm/appearance.lua
+++ home/.config/wezterm/appearance.lua
@@ -5,7 +5,7 @@ Wez_Conf.font = wezterm.font "Berkeley Mono"
 Wez_Conf.font_size = 14.5
 
 -- colors
-Wez_Conf.color_scheme = 'midnight-in-mojave'
+Wez_Conf.color_scheme = 'WildCherry'
 
 -- tab bar
 Wez_Conf.enable_tab_bar = true
--- home/.config/wezterm/remap.lua
+++ home/.config/wezterm/remap.lua
@@ -154,3 +154,33 @@ table.insert(Wez_Conf.keys, {
     )
   end),
 })
+
+
+-- OMG
+-- THEMES
+-- wezterm.GLOBAL.currentSchemeIndex = (wezterm.GLOBAL.currentSchemeIndex or 0) + 1
+--
+--
+-- warning this sets config overrides on the window
+-- so like dont be surprised when your config is overridden on the window for like its whole life
+-- local function themeCycler(window, _)
+--   local allSchemes = wezterm.color.get_builtin_schemes()
+--
+--   local keyset = {}
+--   local n = 0
+--
+--   for k, v in pairs(allSchemes) do
+--     n = n + 1
+--     keyset[n] = k
+--   end
+--
+--   local index = wezterm.GLOBAL.currentSchemeIndex
+--   local overrides = window:get_config_overrides() or {}
+--   local new_scheme = keyset[index]
+--   overrides.color_scheme = new_scheme
+--   wezterm.log_info("Switched to: " .. new_scheme)
+--   window:set_config_overrides(overrides)
+-- end
+--
+-- -- add theme cycle keybind
+-- table.insert(Wez_Conf.keys, { key = "t", mods = "ALT", action = wezterm.action_callback(themeCycler) })
