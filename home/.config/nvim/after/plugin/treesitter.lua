c00c684f7d8e07f9b7629736f11cd7e1859bfde2
cbackas <zgibson@live.com>
2023-11-03 16:30:30 -0500

    do custom filetypes for cf and jenkinsfiles

--- home/.config/nvim/after/plugin/treesitter.lua
+++ home/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = 'all',
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -66,3 +66,7 @@ require('nvim-treesitter.configs').setup {
     enable = true,
   },
 }
+
+-- alias custom filetypes to existing parsers
+vim.treesitter.language.register('yaml', 'cfn-yaml')
+vim.treesitter.language.register('groovy', 'Jenkinsfile')
--- home/.config/nvim/lua/cback/filetypes.lua
+++ home/.config/nvim/lua/cback/filetypes.lua
@@ -1,4 +1,29 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "Jenkinsfile", "*.Jenkinsfile" },
-  command = "set filetype=groovy"
+vim.filetype.add({
+  pattern = {
+    ['.*.Jenkinsfile'] = {
+      priority = math.huge,
+      function(_, _)
+        return 'Jenkinsfile'
+      end,
+    },
+  },
+  filename = {
+    ['Jenkinsfile'] = 'Jenkinsfile'
+  },
+})
+
+
+vim.filetype.add({
+  pattern = {
+    ['.*.ya?ml'] = {
+      priority = math.huge,
+      function(_, bufnr)
+        local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
+        if vim.regex([[AWSTemplateFormatVersion]]):match_str(content) ~= nil or
+            vim.regex([[Resources]]):match_str(content) ~= nil then
+          return 'cfn-yaml'
+        end
+      end,
+    },
+  },
 })
