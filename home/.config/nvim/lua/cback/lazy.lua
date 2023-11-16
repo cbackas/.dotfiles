fc1171dac0da5621fbbde0f8506c7c39b465fb29
cbackas <zgibson@live.com>
2023-11-16 07:40:16 -0600

    add telescope symbols

--- home/.config/nvim/lua/cback/lazy.lua
+++ home/.config/nvim/lua/cback/lazy.lua
@@ -107,18 +107,28 @@ require('lazy').setup({
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',         opts = {} },
+  { 'numToStr/Comment.nvim',          opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
-
   {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make',
-    cond = function()
-      return vim.fn.executable 'make' == 1
-    end,
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+
+
+      'nvim-telescope/telescope-symbols.nvim'
+    }
   },
+
 
   "ThePrimeagen/harpoon",
   'ThePrimeagen/git-worktree.nvim',
