aba7fa7e878d89ab0eba68cd4c35010c31768091
cbackas <zgibson@live.com>
2023-12-09 21:50:59 -0600

    add abbreviations

--- home/.config/nvim/lua/cback/set.lua
+++ home/.config/nvim/lua/cback/set.lua
@@ -57,3 +57,14 @@ vim.g.netrw_banner = 0
 
 -- the coverage plugin needs this to find the coverage files
 vim.g.coverage_json_report_path = 'coverage/coverage-final.json'
+
+-- add some usefull abbreviations for the command line
+vim.cmd("cnoreabbrev W! w!")
+vim.cmd("cnoreabbrev Q! q!")
+vim.cmd("cnoreabbrev Qall! qall!")
+vim.cmd("cnoreabbrev Wq wq")
+vim.cmd("cnoreabbrev Wa wa")
+vim.cmd("cnoreabbrev wQ wq")
+vim.cmd("cnoreabbrev WQ wq")
+vim.cmd("cnoreabbrev W w")
+vim.cmd("cnoreabbrev Q q")
