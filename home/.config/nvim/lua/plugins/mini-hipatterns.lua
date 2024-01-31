dce4ebdea3a630a883542773c08be224388c82cc
cbackas <zgibson@live.com>
2024-01-31 08:21:35 -0600

    mini-hipatterns

---
+++ home/.config/nvim/lua/plugins/mini-hipatterns.lua
@@ -0,0 +1,23 @@
+return {
+  'echasnovski/mini.hipatterns',
+  version = '*',
+  config = function()
+    local hipatterns = require('mini.hipatterns')
+    hipatterns.setup({
+      highlighters = {
+        -- Highlight standalone 'TODO', 'NOTE'
+        todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+        note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+
+        -- Highlight hex color strings (`#rrggbb`) using that color
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+      delay = {
+        -- How much to wait for update after every text change
+        text_change = 200,
+        -- How much to wait for update after window scroll
+        scroll = 50,
+      },
+    })
+  end
+}
