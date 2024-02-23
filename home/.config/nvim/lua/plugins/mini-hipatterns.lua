8827bb551579cd3a2bb2ae26e654f02e7ea09270
Zac Gibson <zgibson@securityesys.com>
2024-02-23 09:59:36 -0600

    add todo comments

--- home/.config/nvim/lua/plugins/mini-hipatterns.lua
+++ home/.config/nvim/lua/plugins/mini-hipatterns.lua
@@ -6,8 +6,8 @@ return {
     hipatterns.setup({
       highlighters = {
         -- Highlight standalone 'TODO', 'NOTE'
-        todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-        note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+        -- todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+        -- note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
 
         -- Highlight hex color strings (`#rrggbb`) using that color
         hex_color = hipatterns.gen_highlighter.hex_color(),
