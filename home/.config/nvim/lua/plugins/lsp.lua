f290f6a4dbaae048a373b6efb1d94b40e2e35594
cbackas <zgibson@live.com>
2024-03-11 10:00:56 -0500

    dont conform if no eslint config

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -192,6 +192,14 @@ local init = function()
       lsp_fallback = true,
     },
   })
+  require("conform.formatters.eslint_d").cwd = require("conform.util").root_file({
+    ".eslint.js",
+    ".eslint.cjs",
+    ".eslint.yaml",
+    ".eslint.yml",
+    ".eslint.json",
+  })
+  require("conform.formatters.eslint_d").require_cwd = true
 end
 
 return {
