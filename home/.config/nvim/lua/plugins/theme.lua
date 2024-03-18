78521213a800757110bafb6694c0bef8a36dcabc
cbackas <zgibson@live.com>
2024-03-17 20:46:02 -0500

    dont let rocky ruin your life

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -3,6 +3,7 @@ return {
   dependencies = { "rktjmp/lush.nvim" },
   name = "arctic",
   branch = "main",
+  commit = "09e2298",
   priority = 1000,
   config = function()
     vim.cmd("colorscheme my-arctic")
