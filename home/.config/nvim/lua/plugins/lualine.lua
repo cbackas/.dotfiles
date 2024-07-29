3296d8079b888e959f9fc00de52e9c04e2c2ead1
cbackas <zgibson@live.com>
2024-07-28 23:43:08 -0500

    make oil folders from harpoon work in lualine tabline

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -85,12 +85,21 @@ local config = function()
     local current_file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":.")
     for index = 1, marks_length do
       local harpoon_file_path = harpoon:list():get(index).value
-      local file_name = harpoon_file_path == "" and "(empty)" or vim.fn.fnamemodify(harpoon_file_path, ':t')
 
+      local label = ""
+      if vim.startswith(harpoon_file_path, "oil") then
+        local dir_path = string.sub(harpoon_file_path, 7)
+        dir_path = vim.fn.fnamemodify(dir_path, ":.")
+        label = '[' .. dir_path .. ']'
+      elseif harpoon_file_path ~= "" then
+        label = vim.fn.fnamemodify(harpoon_file_path, ":t")
+      end
+
+      label = label ~= "" and label or "(empty)"
       if current_file_path == harpoon_file_path then
-        contents[index] = string.format("%%#HarpoonNumberActive# %s. %%#HarpoonActive#%s ", index, file_name)
+        contents[index] = string.format("%%#HarpoonNumberActive# %s. %%#HarpoonActive#%s ", index, label)
       else
-        contents[index] = string.format("%%#HarpoonNumberInactive# %s. %%#HarpoonInactive#%s ", index, file_name)
+        contents[index] = string.format("%%#HarpoonNumberInactive# %s. %%#HarpoonInactive#%s ", index, label)
       end
     end
 
