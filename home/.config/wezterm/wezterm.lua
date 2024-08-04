60e3e03f61099edf9a73e0e0f75f5afe6015b07a
cbackas <zgibson@live.com>
2024-08-04 00:10:49 -0500

    wezterm types in nvim

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 
 -- Wez_Conf is a global variable that holds all the configuration
+---@class _.wezterm.ConfigBuilder
 _G.Wez_Conf = wezterm.config_builder()
 
 require('global_utils')
@@ -10,4 +11,7 @@ require("set")
 require("appearance")
 require("remap")
 
+-- This cast is needed for other config files to end up using the proper WezTerm
+-- types rather than the type of the value returned by this file
+---@cast Wez_Conf WezTerm
 return Wez_Conf
