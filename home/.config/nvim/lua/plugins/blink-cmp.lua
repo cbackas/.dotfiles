3ed1eab4ea81b17f07fe1dd96197b00912cb5959
cbackas <zgibson@live.com>
2024-12-19 01:22:28 -0600

    stop blink from making copilot not work

--- home/.config/nvim/lua/plugins/blink-cmp.lua
+++ home/.config/nvim/lua/plugins/blink-cmp.lua
@@ -10,10 +10,16 @@ return {
   ---@type blink.cmp.Config
   opts = {
     keymap = {
-      preset = 'default',
+      ['<C-y>'] = { 'fallback' },
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-e>'] = { 'hide' },
       ['<C-CR>'] = { 'select_and_accept' },
+      ['<C-p>'] = { 'select_prev', 'fallback' },
+      ['<C-n>'] = { 'select_next', 'fallback' },
       ['<C-d>'] = { 'scroll_documentation_up', 'fallback' },
       ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<Tab>'] = { 'snippet_forward', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
     },
 
     appearance = {
