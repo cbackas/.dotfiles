aecc4b700fb559b87690a2d1405767e524211d65
cbackas <zgibson@live.com>
2024-12-19 22:31:25 -0600

    finish transition off of telescope

--- home/.config/nvim/lua/plugins/fzf.lua
+++ home/.config/nvim/lua/plugins/fzf.lua
@@ -1,38 +1,41 @@
 local function init()
   local fzf = require('fzf-lua')
 
-  vim.keymap.set('n', '<leader>?', fzf.oldfiles, { desc = 'Find recently opened files' })
-  -- vim.keymap.set('n', '<leader><space>', fzf.buffers, { desc = '[ ] Find existing buffers' })
-  vim.keymap.set('n', '<leader>/', function()
-    fzf.lgrep_curbuf()
-  end, { desc = 'Fuzzily search in current buffer' })
-
-  vim.keymap.set('n', '<leader>sh', function()
-    fzf.help_tags()
-  end, { desc = '[S]earch [H]elp' })
-
-  vim.keymap.set('n', '<leader>sw', function()
-    fzf.grep_cword()
-  end, { desc = '[S]earch current [W]ord' })
-
-  vim.keymap.set('n', '<leader>sg', function()
-    fzf.live_grep()
-  end, { desc = '[S]earch by [G]rep' })
-
-  vim.keymap.set('n', '<leader>sf', function()
-    fzf.files()
-  end, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<leader>?', function()
+    fzf.oldfiles({
+      cwd_only = true,
+      -- ensure files exist
+      stat_file = true,
+    })
+  end, { desc = 'Find recently opened files' })
+  vim.keymap.set('n', '<leader>/', fzf.lgrep_curbuf, { desc = 'Fuzzily search in current buffer' })
+  vim.keymap.set('n', '<leader>sh', fzf.help_tags, { desc = '[S]earch [H]elp' })
+  vim.keymap.set('n', '<leader>sw', fzf.grep_cword, { desc = '[S]earch current [W]ord' })
+  vim.keymap.set('n', '<leader>sb', fzf.buffers, { desc = '[S] Find existing [B]uffers' })
+  vim.keymap.set('n', '<leader>sg', fzf.live_grep, { desc = '[S]earch by [G]rep' })
+  vim.keymap.set('n', '<leader>sf', fzf.files, { desc = '[S]earch [F]iles' })
 
   -- TODO how do you shove a search term into fzf.files?
   -- vim.keymap.set('n', '<leader>so', function()
   --   fzf.files({ vim.fn.expand("%:t:r") })
   -- end, { desc = '[S]earch [O]ther' })
+
+  -- INFO use fzf as the default ui selector
+  fzf.register_ui_select(function(_, items)
+    local min_h, max_h = 0.15, 0.70
+    local h = (#items + 4) / vim.o.lines
+    if h < min_h then
+      h = min_h
+    elseif h > max_h then
+      h = max_h
+    end
+    return { winopts = { height = h, width = 0.60, row = 0.40 } }
+  end)
 end
 
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   init = init,
-  opts = {
-  },
+  opts = {},
 }
