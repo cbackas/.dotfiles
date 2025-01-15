bad2d84d0a21b2eeecd015d8e5d3273c74dab40b
cbackas <git@zac.gg>
2025-01-15 12:11:02 -0600

    load snacks types into lazydev

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -253,6 +253,7 @@ return {
       opts = {
         library = {
           { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          { path = 'snacks.nvim',        words = { "snacks", "Snacks" } },
         },
       },
     },
