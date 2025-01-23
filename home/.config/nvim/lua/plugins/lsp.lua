895f334421c4a68929fc5f5750dd741a360407d2
cbackas <git@zac.gg>
2025-01-23 16:06:12 -0600

    move sonarlint config into `cond` function

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -250,30 +250,6 @@ local init = function()
   })
 end
 
-local sonarlint_config = function()
-  if os.getenv("USE_SONARLINT") ~= "true" then
-    return
-  end
-
-  return {
-    "https://gitlab.com/cbackas/sonarlint.nvim.git",
-    opts = {
-      server = {
-        cmd = {
-          vim.fn.expand("~/.local/share/nvim/mason/packages/sonarlint-language-server/sonarlint-language-server"),
-          '-stdio',
-          '-analyzers',
-          vim.fn.expand("~/.local/share/nvim/mason/share/sonarlint-analyzers/sonarjs.jar"),
-        },
-      },
-      filetypes = {
-        "typescript",
-        "javascript"
-      }
-    }
-  }
-end
-
 return {
   'neovim/nvim-lspconfig',
   init = init,
@@ -307,6 +283,27 @@ return {
       opts = {},
     },
     'saghen/blink.cmp',
-    sonarlint_config()
+    {
+      "https://gitlab.com/cbackas/sonarlint.nvim.git",
+      opts = {
+        server = {
+          cmd = {
+            vim.fn.expand("~/.local/share/nvim/mason/packages/sonarlint-language-server/sonarlint-language-server"),
+            '-stdio',
+            '-analyzers',
+            vim.fn.expand("~/.local/share/nvim/mason/share/sonarlint-analyzers/sonarjs.jar"),
+          },
+        },
+        filetypes = {
+          "typescript",
+          "javascript"
+        }
+      },
+      cond = function()
+        -- if 'USE_SONARLINT' env var isn't true, don't load the plugin
+        local use_sonarlint = os.getenv('USE_SONARLINT')
+        return use_sonarlint == '1' or use_sonarlint == 'yes' or use_sonarlint == 'true'
+      end
+    }
   }
 }
