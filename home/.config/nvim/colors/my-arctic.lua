e5c2eade80850387f3f7d34b502a9a49d8ae6edb
cbackas <git@zac.gg>
2025-02-04 01:41:26 -0600

    add some right click stuff

--- home/.config/nvim/colors/my-arctic.lua
+++ home/.config/nvim/colors/my-arctic.lua
@@ -5,6 +5,7 @@ local lush = require('lush')
 local arctic = require('lush_theme.arctic')
 
 local new_bg = '#1b1f25'
+local popup_bg = '#2E2E33'
 
 local spec = lush.extends({ arctic }).with(function()
   -- basically replaced all instances of the background var being used with new_bg
@@ -15,7 +16,8 @@ local spec = lush.extends({ arctic }).with(function()
     Normal { fg = arctic.Normal.fg, bg = new_bg },
     TabLineSel { fg = arctic.TabLineSel.fg, bg = new_bg },
     Todo { fg = new_bg, bg = arctic.Todo.bg, gui = arctic.Todo.gui },
-    Pmenu { fg = arctic.Pmenu.fg, bg = new_bg },
+    Pmenu { fg = arctic.Pmenu.fg, bg = popup_bg },
+    SnacksPicker { fg = arctic.Pmenu.fg, bg = new_bg },
   }
 end)
 
