a13a57ab4e372a279b640aa04a6c4f8a768dc184
cbackas <git@zac.gg>
2025-02-06 00:29:53 -0600

    Revert "update statuscolumn to use newish %l"
    This reverts commit 7108d8c7d05ec82507e087989febedf7f1b6bff2.

--- home/.config/nvim/lua/cback/set.lua
+++ home/.config/nvim/lua/cback/set.lua
@@ -23,10 +23,11 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 vim.opt.cursorline = true
-vim.wo.signcolumn = 'yes:1'
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.statuscolumn = '%s%l' ..
+vim.wo.signcolumn = 'yes'
+vim.opt.number = false
+vim.opt.relativenumber = false
+vim.opt.statuscolumn = '%s' ..
+    '%=%{printf("%4d", v:relnum?v:relnum:v:lnum)}' ..
     '%{foldlevel(v:lnum)>0?(foldclosed(v:lnum)==-1?" ":"▸"):" "} '
 
 -- Decrease update time
