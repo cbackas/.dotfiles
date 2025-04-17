4a61ec9647e8ff560dfa4eb1dce80e1c6eb00f97
cbackas <git@zac.gg>
2025-04-17 10:28:03 -0500

    add kulala

--- home/.config/nvim/lua/plugins/conform.lua
+++ home/.config/nvim/lua/plugins/conform.lua
@@ -10,6 +10,14 @@ return {
       htmldjango = { 'djlint' },
       -- ['cfn-yaml'] = { 'yamlfmt' },
       sh = { 'shellcheck', 'shfmt' },
+      http = { "kulala" },
+    },
+    formatters = {
+      kulala = {
+        command = "kulala-fmt",
+        args = { "format", "$FILENAME" },
+        stdin = false,
+      },
     },
     format_on_save = {
       timeout_ms = 500,
