097d903f84be0b13594b4815589b312a23c26aa0
cbackas <git@zac.gg>
2026-01-14 18:24:41 -0600

    cont let jsonls format with conform

--- home/.config/nvim/lua/plugins/conform.lua
+++ home/.config/nvim/lua/plugins/conform.lua
@@ -2,6 +2,10 @@ local function not_vtsls(client)
   return client.name ~= "vtsls" and client.name ~= "vue_ls"
 end
 
+local function not_jsonls(client)
+  return client.name ~= "jsonls"
+end
+
 return {
   'stevearc/conform.nvim',
   event = { 'BufRead', 'BufNewFile' },
@@ -9,6 +13,7 @@ return {
     formatters_by_ft = {
       javascript = { 'eslint', lsp_format = 'first', stop_after_first = true, filter = not_vtsls },
       typescript = { 'eslint', lsp_format = 'first', stop_after_first = true, filter = not_vtsls },
+      json = { filter = not_jsonls },
       vue = { 'eslint', lsp_format = 'first', stop_after_first = true, filter = not_vtsls },
       -- html = { 'eslint_d' },
       ['jinja.html'] = { 'djlint' },
