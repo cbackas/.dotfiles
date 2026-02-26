74a4c1c5b2244810da07ce03a2497c1963a168a6
Zac Gibson <zac.gibson@lge.com>
2026-02-25 20:59:32 -0600

    split super key nav into a separate file

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
-local wezterm = require('wezterm') --[[@as Wezterm]]
+local wezterm = require("wezterm") --[[@as Wezterm]]
 
 -- Wez_Conf is a global variable that holds all the configuration
 _G.Wez_Conf = wezterm.config_builder()
 
-require('global_utils')
+require("global_utils")
 
 -- build up the Wez_Conf values from the other files
 require("settings")
@@ -12,6 +12,7 @@ require("appearance")
 require("remap_keys")
 require("remap_mouse")
 
+require("features/super_key_nav")
 require("features/f_key_nav")
 require("features/project_picker")
 require("features/mux_pane_nav")
