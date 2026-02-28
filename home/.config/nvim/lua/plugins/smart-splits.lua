eaa4542c5eba66116bb113ce478099081dfff6b1
cbackas <git@zac.gg>
2026-02-28 11:25:31 -0600

    fix smart splits

--- home/.config/nvim/lua/plugins/smart-splits.lua
+++ home/.config/nvim/lua/plugins/smart-splits.lua
@@ -6,10 +6,10 @@ local init = function()
   vim.keymap.set('n', '<A-B>', require('smart-splits').resize_right)
 
   -- moving between splits
-  vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_left)
-  vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
-  vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
-  vim.keymap.set('n', '<C-B>', require('smart-splits').move_cursor_right, { noremap = true })
+  vim.keymap.set({ 'n', 't' }, '<C-l>', require('smart-splits').move_cursor_left)
+  vim.keymap.set({ 'n', 't' }, '<C-j>', require('smart-splits').move_cursor_down)
+  vim.keymap.set({ 'n', 't' }, '<C-k>', require('smart-splits').move_cursor_up)
+  vim.keymap.set({ 'n', 't' }, '<C-B>', require('smart-splits').move_cursor_right, { noremap = true })
 end
 
 return {
